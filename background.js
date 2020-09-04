37f75cd6f8342128a19923dfc5abebfbe774fd31
cielpy <beijiu572@gmail.com>
2020-09-04 16:40:35 +0800

    减少一次跳转等待

--- background.js
+++ background.js
@@ -8,17 +8,17 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(details)
     if (details.url.startsWith('https://apps.apple.com/')) {
         // console.log(details.url.split('/'))
         url_arr = details.url.split('/')
+
         if (url_arr[3] === 'app') {
             // https://apps.apple.com/app/apple-store/id1470168007
             url_arr.insert(3, 'cn')
             chrome.tabs.update(details.tabId, {url: url_arr.join('/')});
         }
-        else  if (url_arr[3] !== 'cn'){
+
+        if (url_arr[3] !== 'cn'){
             // https://apps.apple.com/us/app/spark-mail-email-by-readdle/id997102246
             url_arr[3] = 'cn'
             chrome.tabs.update(details.tabId, {url: url_arr.join('/')});
         }
-
     }
-
 });
