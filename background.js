4d5ab161ffe8fd00003136cd4d70d0773988080c
cielpy <beijiu572@gmail.com>
2020-09-04 13:13:33 +0800

    优化体验，不需要等待 404 了

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 Array.prototype.insert = function ( index, item ) {
     this.splice( index, 0, item );
 };
-chrome.webNavigation.onErrorOccurred.addListener(function(details) 
+chrome.webNavigation.onBeforeNavigate.addListener(function(details) 
 {
     if (details.frameId != 0) //ignore subframes. 0 is main frame
     { return; }
@@ -11,14 +11,14 @@ chrome.webNavigation.onErrorOccurred.addListener(function(details)
         if (url_arr[3] === 'app') {
             // https://apps.apple.com/app/apple-store/id1470168007
             url_arr.insert(3, 'cn')
-
+            chrome.tabs.update(details.tabId, {url: url_arr.join('/')});
         }
-        else {
+        else  if (url_arr[3] !== 'cn'){
             // https://apps.apple.com/us/app/spark-mail-email-by-readdle/id997102246
             url_arr[3] = 'cn'
+            chrome.tabs.update(details.tabId, {url: url_arr.join('/')});
         }
 
-        chrome.tabs.update(details.tabId, {url: url_arr.join('/')});
     }
 
 });
